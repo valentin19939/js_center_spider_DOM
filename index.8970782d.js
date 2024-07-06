5e7855664a6c82a9d837c98d7ab87ea1271e7514
valentyn19939 <valik.hai1993@gmail.com>
2024-07-06 11:38:42 +0200

    make build

---
+++ index.8970782d.js
@@ -0,0 +1,2 @@
+var e=document.querySelector(".wall");document.querySelector(".spider").style.position="relative",e.style.display="flex",e.style.justifyContent="center",e.style.alignItems="center";
+//# sourceMappingURL=index.8970782d.js.map
